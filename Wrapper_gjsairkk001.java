12f274131135e2345698947ed364bcb36e05b519
liuxxinbeijing <545285776@qq.com>
2014-07-15 22:50:46 +0800

    Update Wrapper_gjsairkk001.java

--- Wrapper_gjsairkk001.java
+++ Wrapper_gjsairkk001.java
@@ -13,6 +13,7 @@ import org.apache.commons.httpclient.cookie.CookiePolicy;
 import org.apache.commons.httpclient.protocol.Protocol;
 import org.apache.commons.lang.StringUtils;
 
+import com.qunar.qfwrapper.bean.booking.BookingInfo;
 import com.qunar.qfwrapper.bean.booking.BookingResult;
 import com.qunar.qfwrapper.bean.search.FlightDetail;
 import com.qunar.qfwrapper.bean.search.FlightSearchParam;
